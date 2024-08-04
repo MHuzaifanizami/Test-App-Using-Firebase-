ac400498eef76927b471dda3d460a94599d285bc
M Huzaifa Nizami <mhuzaifanizami@gmail.com>
2024-08-04 14:05:01 +0500

    file

--- app.js
+++ app.js
@@ -1,80 +1,79 @@
-import { auth, createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signOut, sendEmailVerification, GoogleAuthProvider, provider, signInWithPopup, RecaptchaVerifier, signInWithPhoneNumber  } from "./firebase.js";
 
 
-onAuthStateChanged(auth, (user) => {
-    if (user) {
-        console.log("user", user)
+// onAuthStateChanged(auth, (user) => {
+//     if (user) {
+//         console.log("user", user)
 
-    } else {
-        console.log("user not exist")
-    }
-});
-
-
-let signUp = () => {
-    let email = document.getElementById("email");
-    let password = document.getElementById("password");
-
-    signInWithEmailAndPassword(auth, email.value, password.value)
-        .then((res) => {
-            console.log("res", res.user)
-        })
-        .catch((error) => {
-            console.log("error", error.message)
-        });
-}
-
-let signupBtn = document.getElementById("signupBtn");
-signupBtn.addEventListener("click", signUp)
+//     } else {
+//         console.log("user not exist")
+//     }
+// });
 
 
-let logOut = () => {
-    signOut(auth).then(() => {
-        console.log("log Out Successfully")
-    }).catch((error) => {
-        console.log("error")
-    })
-};
+// let signUp = () => {
+//     let email = document.getElementById("email");
+//     let password = document.getElementById("password");
 
-let logoutBtn = document.getElementById("logoutBtn");
-logoutBtn.addEventListener("click", logOut)
+//     signInWithEmailAndPassword(auth, email.value, password.value)
+//         .then((res) => {
+//             console.log("res", res.user)
+//         })
+//         .catch((error) => {
+//             console.log("error", error.message)
+//         });
+// }
 
-let sendCode = () => {
-
-    sendEmailVerification(auth.currentUser)
-        .then(() => {
-            console.log("sent")
-        });
-}
-
-let verifyBtn = document.getElementById("verifyBtn");
-verifyBtn.addEventListener("click", sendCode)
+// let signupBtn = document.getElementById("signupBtn");
+// signupBtn.addEventListener("click", signUp)
 
 
-let verifyGoogke = () => {
-    signInWithPopup(auth, provider)
-        .then((result) => {
+// let logOut = () => {
+//     signOut(auth).then(() => {
+//         console.log("log Out Successfully")
+//     }).catch((error) => {
+//         console.log("error")
+//     })
+// };
 
-            const credential = GoogleAuthProvider.credentialFromResult(result);
-            const token = credential.accessToken;
-            const user = result.user;
-            console.log("token", token)
-            console.log("user", user)
+// let logoutBtn = document.getElementById("logoutBtn");
+// logoutBtn.addEventListener("click", logOut)
 
-        }).catch((error) => {
-            const errorCode = error.code;
-            const errorMessage = error.message;
-            const email = error.customData.email;
-            const credential = GoogleAuthProvider.credentialFromError(error);
-            console.log("errorCode" , errorCode)
-            console.log("credential" , credential)
+// let sendCode = () => {
 
-        });
-}
+//     sendEmailVerification(auth.currentUser)
+//         .then(() => {
+//             console.log("sent")
+//         });
+// }
+
+// let verifyBtn = document.getElementById("verifyBtn");
+// verifyBtn.addEventListener("click", sendCode)
 
 
-let verifyGoogle = document.getElementById("verifyGoogle");
-verifyGoogle.addEventListener("click", verifyGoogke)
+// let verifyGoogke = () => {
+//     signInWithPopup(auth, provider)
+//         .then((result) => {
+
+//             const credential = GoogleAuthProvider.credentialFromResult(result);
+//             const token = credential.accessToken;
+//             const user = result.user;
+//             console.log("token", token)
+//             console.log("user", user)
+
+//         }).catch((error) => {
+//             const errorCode = error.code;
+//             const errorMessage = error.message;
+//             const email = error.customData.email;
+//             const credential = GoogleAuthProvider.credentialFromError(error);
+//             console.log("errorCode" , errorCode)
+//             console.log("credential" , credential)
+
+//         });
+// }
+
+
+// let verifyGoogle = document.getElementById("verifyGoogle");
+// verifyGoogle.addEventListener("click", verifyGoogke)
 
 
 
@@ -121,3 +120,121 @@ verifyGoogle.addEventListener("click", verifyGoogke)
 
 // let verifyBtn = document.getElementById("verifyBtn")
 // verifyBtn.addEventListener("click", Verify)
+
+// add data in firebase storage 
+
+import { auth, createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signOut, sendEmailVerification, GoogleAuthProvider, provider, signInWithPopup, RecaptchaVerifier, signInWithPhoneNumber, doc, setDoc, db, getDoc, collection, addDoc, updateDoc, onSnapshot , deleteDoc  } from "./firebase.js";
+
+// let signUp = () => {
+
+//     let name = document.getElementById("name");
+//     let age = document.getElementById("age");
+//     let address = document.getElementById("address");
+//     let email = document.getElementById("email");
+//     let password = document.getElementById("password");
+
+
+// let userData = {
+//     name: name.value,
+//     age: age.value,
+//     address: address.value,
+//     password: password.value
+// }
+
+//     createUserWithEmailAndPassword(auth, email.value, password.value)
+//         .then(async (res) => {
+//             console.log("res", res.user);
+
+//             const washingtonRef = doc(db, "cities", "DC");
+//             await updateDoc(washingtonRef, {
+//             capital: true
+// });
+
+
+// const docRef = await addDoc(collection(db, "users"), {
+//     name: "syrup",
+//     country: "pakistan"
+//   });
+//   console.log("Document written with ID: ", docRef.id);
+
+
+// await setDoc(doc(db, "users", res.user.uid), userData);
+
+//             console.log("user added")
+//         })
+
+//         .catch((error) => {
+//             console.log("error", error.message)
+//         });
+// }
+// let signupBtn = document.getElementById("signupBtn");
+// signupBtn.addEventListener("click", signUp)
+
+// get data from data base ################################
+
+
+
+// let logIn = () => {
+//     let email = document.getElementById("email");
+//     let password = document.getElementById("password");
+
+//     signInWithEmailAndPassword(auth, email.value, password.value)
+//     .then(async (res) => {
+//         console.log("res", res.user);
+//         const docSnap = await getDoc (doc(db, "users", res.user.uid));
+//         console.log("data ==>" , docSnap.exists())
+//         // console.log("user added")
+//     })
+
+//     .catch((error) => {
+//         console.log("error", error.message)
+//     });
+
+// }
+
+// let logInBtn = document.getElementById("logInBtn")
+// logInBtn.addEventListener("click", logIn)
+
+
+//     ////////////////     CHAT         /  /////////////////////////////////////
+let list = document.getElementById("list")
+let add = async () => {
+    let search = document.getElementById("search")
+    let ref = collection(db, "todos");
+    await addDoc(ref, {
+        todo: search.value
+    })
+    console.log("todo added");
+    search.value = ""
+};
+
+
+let getTodo = () => {
+    onSnapshot(collection(db, "todos"), (snapshot) => {
+        list.innerText = ""
+        snapshot.forEach((doc) => {
+            console.log(doc.data() , doc.id);
+            let { todo } = doc.data();
+            list.innerHTML += `<li>${todo} <button onclick="delTodo('${doc.id}')">del</button></li>`
+        });
+      });
+}
+
+getTodo()
+
+let delTodo = async (id) => {
+console.log(id)
+
+await deleteDoc(doc(db , "todos" , id))
+console.log("deleted")
+}
+window.delTodo = delTodo;
+
+
+
+
+
+
+
+let addTodo = document.getElementById("addTodo");
+addTodo.addEventListener("click", add)
--- firebase.js
+++ firebase.js
@@ -1,34 +1,56 @@
 
- import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-app.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-app.js";
 
- import { getAuth , createUserWithEmailAndPassword , onAuthStateChanged ,signInWithEmailAndPassword , signOut , sendEmailVerification , GoogleAuthProvider , signInWithPopup , RecaptchaVerifier , signInWithPhoneNumber  } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-auth.js";
-
-
- const firebaseConfig = {
-   apiKey: "AIzaSyBTs1ARZtcrZ38yzFphzQA0TIFvvlLEr5Q",
-   authDomain: "huzaifa-9289a.firebaseapp.com",
-   projectId: "huzaifa-9289a",
-   storageBucket: "huzaifa-9289a.appspot.com",
-   messagingSenderId: "565186540599",
-   appId: "1:565186540599:web:92183b57c6107e2306bbb2"
- };
+import { getAuth, createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signOut, sendEmailVerification, GoogleAuthProvider, signInWithPopup, RecaptchaVerifier, signInWithPhoneNumber, } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-auth.js";
 
 
- const app = initializeApp(firebaseConfig);
- const auth = getAuth(app);
- const provider = new GoogleAuthProvider();
 
- export {
-    auth,
-    createUserWithEmailAndPassword ,
-    onAuthStateChanged ,
-    signInWithEmailAndPassword ,
-    signOut ,
-    sendEmailVerification ,
-    GoogleAuthProvider ,
-    provider,
-    signInWithPopup,
-    RecaptchaVerifier ,
-    signInWithPhoneNumber,
-    
- }
+const firebaseConfig = {
+  apiKey: "AIzaSyBTs1ARZtcrZ38yzFphzQA0TIFvvlLEr5Q",
+  authDomain: "huzaifa-9289a.firebaseapp.com",
+  projectId: "huzaifa-9289a",
+  storageBucket: "huzaifa-9289a.appspot.com",
+  messagingSenderId: "565186540599",
+  appId: "1:565186540599:web:92183b57c6107e2306bbb2"
+};
+
+
+
+
+
+
+
+
+
+import { getFirestore, doc, setDoc, getDoc, collection, addDoc , updateDoc , onSnapshot , deleteDoc  } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-firestore.js"
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+const db = getFirestore(app);
+
+
+export {
+  auth,
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signOut,
+  sendEmailVerification,
+  GoogleAuthProvider,
+  provider,
+  signInWithPopup,
+  RecaptchaVerifier,
+  signInWithPhoneNumber,
+  doc,
+  setDoc,
+  db,
+  getDoc,
+  collection,
+  addDoc,
+  updateDoc ,
+  onSnapshot,
+  deleteDoc
+}
+
+//////////////////////////////////////////////////////////////////
